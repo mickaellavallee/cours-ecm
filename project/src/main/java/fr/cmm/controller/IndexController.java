69396c277da0377f388203b3b5b37621e6a29547
Mickael Lavallee <lavallee.mickael@gmail.com>
2018-10-12 13:09:29 +0200

    MVC-7

--- project/src/main/java/fr/cmm/controller/IndexController.java
+++ project/src/main/java/fr/cmm/controller/IndexController.java
@@ -91,5 +91,15 @@ public class IndexController {
     public String mentionsLegales() {
         return "mentions-legales";
     }
+
+    @RequestMapping("/404")
+    public String error404() {
+        return "404";
+    }
+
+    @RequestMapping("/500")
+    public String error500() {
+        return "500";
+    }
 }
 
