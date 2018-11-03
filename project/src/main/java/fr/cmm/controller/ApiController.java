9243322f3891591b08d6c9562719b5d1d760c36d
Mickael Lavallee <lavallee.mickael@gmail.com>
2018-11-03 18:07:56 +0100

    REST-API creation de Apicontroller

---
+++ project/src/main/java/fr/cmm/controller/ApiController.java
@@ -0,0 +1,32 @@
+package fr.cmm.controller;
+
+import com.sun.tools.javac.util.DefinedBy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import fr.cmm.service.RecipeService;
+import javax.inject.Inject;
+import javax.management.openmbean.CompositeData;
+
+@Controller
+public class ApiController{
+
+    @Inject
+    private RecipeService recipeService;
+    
+    @RequestMapping("/api/recipes")
+    public String liste(){
+        return "Liste";
+    }
+
+    @RequestMapping("/api/recipes/{id}")
+    public String recette(){
+        return "Recette";
+    }
+
+
+}
+
+
